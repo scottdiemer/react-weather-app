2d37f61a7b818922e7fbfa6009134a10e110522f
scottdiemer <sdiemer@cfl.rr.com>
2018-11-07 16:08:01 -0500

    added styling and converted kelvin temp to fahrenheit

--- src/App.js
+++ src/App.js
@@ -24,9 +24,10 @@ class App extends Component {
     const country = e.target.elements.country.value
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&id=524901&APPID=${API_KEY}`)
     const data = await api_call.json()
+    const convertKelvinToFahrenheit = kelvin => Number(kelvin) * 9/5 - 459.67
     if (city && country) {
       this.setState({
-        temperature: data.main.temp,
+        temperature: convertKelvinToFahrenheit(data.main.temp).toFixed(2),
         city: data.name,
         country: data.sys.country,
         humidity: data.main.humidity,
@@ -47,20 +48,31 @@ class App extends Component {
 
   render () {
     return (
-      <div>
-        <Titles />
-        <Form getWeather={this.getWeather}/>
-        <Weather
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
-          error={this.state.error}
-        />
+      <div className="wrapper">
+        <div className="main">
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-5 title-container">
+                <Titles />
+              </div>
+              <div className="col-xs-7 form-container">
+                <Form getWeather={this.getWeather}/>
+                <Weather
+                  temperature={this.state.temperature}
+                  city={this.state.city}
+                  country={this.state.country}
+                  humidity={this.state.humidity}
+                  description={this.state.description}
+                  error={this.state.error}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
+
 
 export default App
